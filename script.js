49b41a54a3b4df88d527187032ddd9761f99f16b
devarsh2395 <devarsh2395@gmail.com>
2023-01-26 22:18:02 -0500

    updated code

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
+// Current Day function selects the element with id and sets its text to current day
+
 $(document).ready(function(){
     const currentDay = moment().format('dddd MMMM Do YYYY');
     $("#currentDay").text(currentDay);
     updateTimeBlocks();
     loadPlanner();
+
+
+    // Time-block class iterates over each element and assigns the appropriate class based on the hour of the time-block and the current hour
 
     function updateTimeBlocks() {
         const currentHour = moment().hours();
@@ -11,6 +16,8 @@ $(document).ready(function(){
             updateTimeBlockClass(timeBlockHour, currentHour, this);
         });
     }
+
+// assigns the necessary class to the tim-block based on the hour of the time-block and current hour
 
     function updateTimeBlockClass(timeBlockHour, currentHour, timeBlock) {
         if (timeBlockHour > currentHour) {
@@ -22,11 +29,17 @@ $(document).ready(function(){
         }
     }
 
+
+    // adds an event listener to the element. And saves the time and plan to local storage
+
     $(".saveBtn").on("click", function() {
         const time = $(this).siblings(".hour").text();
         const plan = $(this).siblings(".plan").val();
         localStorage.setItem(time, plan);
     });
+
+
+    // Loads the saved plans from local storage and sets the corresponding text area to the saved plan
 
     function loadPlanner() {
         $(".hour").each(function() {
@@ -36,6 +49,7 @@ $(document).ready(function(){
         });
     }
 
+    // Sets the plan for the given hour
     function setPlan(currHour, currPlan) {
         if(currPlan !== null) {
             $(`#${currHour} .plan`).val(currPlan);
