e32b663ff35f0c533e3c54c4bdd377aa889903d0
devarsh2395 <devarsh2395@gmail.com>
2023-01-26 22:02:22 -0500

    Updated code

--- script.js
+++ script.js
@@ -1,48 +1,44 @@
-
 $(document).ready(function(){
-    // current day is displayed at the top of the calendar
-    $("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
-    
-    // each time block is color-coded to indicate whether it is in the past, present, or future
-    function updateTimeBlocks() {
-        var currentHour = moment().hours();
-        
-    
-        $(".time-block").each(function() {
-            var timeBlockHour = parseInt($(this).attr("id"));
-    
-            if (timeBlockHour > currentHour) {
-                $(this).addClass("future");
-            } else if (timeBlockHour === currentHour) {
-                $(this).addClass("present");
-            } else {
-                $(this).addClass("past");
-            }
-        });
-    }
-    
-    // WHEN I click the save button for that time block
-    $(".saveBtn").on("click", function() {
-        var time = $(this).siblings(".hour").text();
-        var plan = $(this).siblings(".plan").val();
-    
-        // THEN the text for that event is saved in local storage
-        localStorage.setItem(time, plan);
-    });
-    
-    // WHEN I refresh the page
-    // THEN the saved events persist
-    function loadPlanner() {
-        $(".hour").each(function() {
-            var currHour = $(this).text();
-            var currPlan = localStorage.getItem(currHour);
-    
-            if(currPlan !== null) {
-                $(this).siblings(".plan").val(currPlan);
-            }
-        });
-    }
-    
+    const currentDay = moment().format('dddd MMMM Do YYYY');
+    $("#currentDay").text(currentDay);
     updateTimeBlocks();
     loadPlanner();
+
+    function updateTimeBlocks() {
+        const currentHour = moment().hours();
+        $(".time-block").each(function() {
+            const timeBlockHour = parseInt(this.id);
+            updateTimeBlockClass(timeBlockHour, currentHour, this);
+        });
+    }
+
+    function updateTimeBlockClass(timeBlockHour, currentHour, timeBlock) {
+        if (timeBlockHour > currentHour) {
+            timeBlock.classList.add("future");
+        } else if (timeBlockHour === currentHour) {
+            timeBlock.classList.add("present");
+        } else {
+            timeBlock.classList.add("past");
+        }
+    }
+
+    $(".saveBtn").on("click", function() {
+        const time = $(this).siblings(".hour").text();
+        const plan = $(this).siblings(".plan").val();
+        localStorage.setItem(time, plan);
+    });
+
+    function loadPlanner() {
+        $(".hour").each(function() {
+            const currHour = this.textContent;
+            const currPlan = localStorage.getItem(currHour);
+            setPlan(currHour, currPlan);
+        });
+    }
+
+    function setPlan(currHour, currPlan) {
+        if(currPlan !== null) {
+            $(`#${currHour} .plan`).val(currPlan);
+        }
+    }
 });
